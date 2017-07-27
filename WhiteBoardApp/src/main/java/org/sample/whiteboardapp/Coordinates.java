98487211ed62b440e3990d3b1271c4211e3a9c11
Trung <trungpq999@gmail.com>
2017-07-27 19:01:21 +0700

    websocket api
    Ứng dụng sử dụng websocket api để truyền tải dữ liệu tới server và server gửi đến các client khác.

---
+++ WhiteBoardApp/src/main/java/org/sample/whiteboardapp/Coordinates.java
@@ -0,0 +1,43 @@
+package org.sample.whiteboardapp;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AdminTN
+ */
+// this class only containts a fields for the x and the y coordinates and some getters and setters
+public class Coordinates {
+
+    private float x;
+    private float y;
+
+    public Coordinates() {
+    }
+
+    public Coordinates(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+}
